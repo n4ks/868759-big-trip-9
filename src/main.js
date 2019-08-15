147121efc60079af9abeb2dd262c33cd285003a7
n4ks <30715874+n4ks@users.noreply.github.com>
2019-08-15 16:47:43 +0300

    в процессе выполнения второго задания

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import { generateNavButtonTemplate } from './components/menu.js';
 import { generateFilterButtonTemplate } from './components/filters.js';
+import { generateCardFilterButtonTemplate } from './components/card-filters.js';
+import { generateCardEditTemplate } from './components/card-edit.js';
 
+// const CARDS_COUNT = 3;
 const controlsElement = document.querySelector(`.trip-controls`);
 const controlsElementHeaders = controlsElement.querySelectorAll(`h2`);
+const tripEventsElement = document.querySelector(`.trip-events`);
 
 const navigationTemplate = `<nav class="trip-controls__trip-tabs  trip-tabs"></nav>`;
 const menuButtons = [
@@ -32,6 +36,29 @@ const filterButtons = [
 
 const filtersTemplate = `<form class="trip-filters" action="#" method="get"></form>`;
 
+const cardFiltersTemplate = `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
+    <span class="trip-sort__item  trip-sort__item--day">Day</span>
+    <span class="trip-sort__item  trip-sort__item--offers">Offers</span>
+  </form>`;
+
+const cardFiltersButtons = [
+  {
+    label: `event`,
+    isChecked: true
+  },
+  {
+    label: `time`,
+    isChecked: false
+  },
+  {
+    label: `price`,
+    isChecked: false
+  }
+];
+
+const tripContainerTemplate = `<ul class="trip-days"></ul>`;
+const tripEventsListTemplate = `<ul class="trip-events__list"></ul>`;
+
 // 1) Создаём контейнер
 const createContainer = (template) => {
   const element = document.createElement(`template`);
@@ -57,10 +84,34 @@ const renderElement = (container, element, position = `beforeEnd`) => {
 const navigationContainer = createContainer(navigationTemplate);
 
 appendToContainer(navigationContainer, generateNavButtonTemplate(menuButtons));
-renderElement(controlsElementHeaders[0], navigationContainer, `afterend`);
+renderElement(controlsElementHeaders[0], navigationContainer, `afterEnd`);
 
 // 'Фильтры'
 const filtersContainer = createContainer(filtersTemplate);
 
 appendToContainer(filtersContainer, generateFilterButtonTemplate(filterButtons));
 renderElement(controlsElementHeaders[1], filtersContainer, `afterEnd`);
+
+// 'Фильтр карточек'
+const cardFiltersContainer = createContainer(cardFiltersTemplate);
+const cardFiltersDecorativeElements = cardFiltersContainer.querySelectorAll(`span`);
+
+appendToContainer(cardFiltersDecorativeElements[0], generateCardFilterButtonTemplate(cardFiltersButtons), `afterEnd`);
+renderElement(tripEventsElement, cardFiltersContainer);
+
+// 'Редактирование карточки'
+const tripEventsContainer = createContainer(tripEventsListTemplate);
+appendToContainer(tripEventsContainer, generateCardEditTemplate());
+
+const tripContainer = createContainer(tripContainerTemplate);
+renderElement(tripContainer, tripEventsContainer);
+renderElement(tripEventsElement, tripContainer);
+
+// 'Карточки'
+// const cardsContainer = createContainer(cardsContainerTemplate);
+
+// for (let i = 1; i <= CARDS_COUNT; i++) {
+//   appendToContainer(cardsContainer, generateCardTemplate(i));
+// }
+
+// renderElement(tripEventsElement, cardsContainer);
